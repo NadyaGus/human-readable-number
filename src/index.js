5d5f32783ce1e225e67cc8e2b8479e1b3c8a9d6e
NadyaGus <ftotkexit@gmail.com>
2023-08-10 07:18:06 +0300

    init: start task human readeble nubmer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function toReadable (number) {
-  
+  let strNumber = nubmer.toString(); // переводит число в строчку (пр. `987`)
+  let reverseStrNumber = ; // сделать реверс строки
 }
